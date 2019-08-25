6f78103c947056da6b1ced50f10ee5f6027edd58
Michał Piechura <piechureq@wp.pl>
2019-08-25 21:37:09 +0200

    install Vundle

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -2,7 +2,15 @@
 echo "Setting up vim!"
 
 cd "$(dirname "$0")"
+
+#install Vundle if not exists
+if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+    echo "Installing Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 cp vimrc ~/.vimrc
+#neovim config
 cp init.vim ~/.config/nvim
 
 echo "Done"
