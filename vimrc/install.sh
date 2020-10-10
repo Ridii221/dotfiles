b88653bfb4c3bae049b8bc724e15c900dc803530
Michał Piechura <piechureq@wp.pl>
2020-10-10 14:16:28 +0200

    now using vim-plug instead of Vundle

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -4,9 +4,16 @@ echo "Setting up vim!"
 cd "$(dirname "$0")"
 
 #install Vundle if not exists
-if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-    echo "Installing Vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+#    echo "Installing Vundle"
+#    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#fi
+
+#install Plug if not exists
+if [ ! -f ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim ]; then
+    echo "Installing Plug"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
 cp vimrc ~/.vimrc
