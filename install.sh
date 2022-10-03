50fdc10abdc7109ccf19bff37c061567e1479222
Michał Piechura <piechureq@wp.pl>
2022-10-03 23:56:27 +0200

    install script fix, icons in nerdtree

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# todo for each dir run install.sh
 sh $HOME/dotfiles/kitty/install.sh
 sh $HOME/dotfiles/vimrc/install.sh
 sh $HOME/dotfiles/zshrc/install.sh
--- vimrc/install.sh
+++ vimrc/install.sh
@@ -3,12 +3,6 @@ echo "Setting up vim!"
 
 cd "$(dirname "$0")"
 
-#install Vundle if not exists
-#if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-#    echo "Installing Vundle"
-#    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#fi
-
 #install Plug if not exists
 if [ ! -f ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim ]; then
     echo "Installing Plug"
