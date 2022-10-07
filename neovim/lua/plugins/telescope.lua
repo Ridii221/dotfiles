10531b1c3996ce226041c3a7694c1a444a594cca
PiechureQ <piechureq@wp.pl>
2022-10-07 20:31:49 +0200

    Telescope fix mappings

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -3,13 +3,13 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', 'ff', ':Telescope<cr>')
 vim.keymap.set('n', 'fi', builtin.find_files)
-vim.keymap.set('n', 'fg', builtin.live_grep)
-vim.keymap.set('n', 'fd', builtin.grep_string)
+vim.keymap.set('n', 'fd', builtin.live_grep)
+vim.keymap.set('n', 'fw', builtin.grep_string)
 vim.keymap.set('n', 'fb', builtin.buffers)
 vim.keymap.set('n', 'fh', builtin.help_tags)
 vim.keymap.set('n', 'fs', builtin.git_status)
-vim.keymap.set('n', 'fC', builtin.git_commits)
-vim.keymap.set('n', 'fc', builtin.git_bcommits)
+vim.keymap.set('n', 'fc', builtin.git_commits)
+vim.keymap.set('n', 'fC', builtin.git_bcommits)
 
 telescope.setup({
     extensions = {
